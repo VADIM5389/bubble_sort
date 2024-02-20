a7b5784c373b3c0f332317474d9390df470c87f5
VADIM5389 <78856791+VADIM5389@users.noreply.github.com>
2024-02-20 08:52:35 +0500

    Update 20.02.2024 Заболотский.py

--- 20.02.2024 Заболотский.py
+++ 20.02.2024 Заболотский.py
@@ -1,23 +1,14 @@
 #def bubble_sort(arr):
-#    n = len(arr)
-#    for i in range(n):
-#        for j in range(0, n-i-1):
-#            if arr[j] > arr[j+1]:
-#                arr[j], arr[j+1] = arr[j+1], arr[j]
-#    return arr
-#
-#arr = [64, 34, 25, 12, 22, 11, 90]
-#print("Исходный список:", arr)
-#sorted_arr = bubble_sort(arr)
-#print("Отсортированный список:", sorted_arr)
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
 
-def naive_search(text, pattern):
-    indices = []
-    for i in range(len(text) - len(pattern) + 1):
-        if text[i: i + len(pattern)] == pattern:
-            indices.append(i)
-    return indices
+arr = [64, 34, 25, 12, 22, 11, 90]
+print("Исходный список:", arr)
+sorted_arr = bubble_sort(arr)
+print("Отсортированный список:", sorted_arr)
 
-text = input("Введите текст")
-pattern = input("Введите текст для поиска")
-print(naive_search(text, pattern))
+
